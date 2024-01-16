e3a2ff30860f91ffc3ce32768888bd66b3a5dc4c
Konstantinos Kazazis <kazaziskonstantinos@gmail.com>
2024-01-16 14:43:18 +0200

    added react router

--- src/components/templates.js
+++ src/components/templates.js
@@ -1,25 +1,27 @@
 import React, {useState} from 'react'
+import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
+
 import Plain from './templates/plain'
 import Home from './home'
 
 function Templates({ details, workExperiences, projects, education, languages, skills }) {
-  const [component, setComponent] = useState(<Home />)
 
-  const changeComponent = (component) => {
-    setComponent(component);
-  }
-    
     return (
+      <Router>
         <div className="bg-gray-200 p-8">
           <nav className='bg-gray-200 w-[100%] border-b-2 border-slate-300 sticky top-0'>
             <ul className='flex justify-between'>
-              <li onClick={() => changeComponent(<Home />)} className='text-xl text-gray-700 hover:bg-white hover:cursor-pointer px-10 py-2'>Home</li>
-              <li onClick={() => changeComponent(<Plain details={details} workExperiences={workExperiences} projects={projects} education={education} languages={languages} skills={skills} />)} className='text-xl text-gray-700 hover:bg-white hover:cursor-pointer px-10 py-2'>Create</li>
-              <li className='text-xl text-gray-700 hover:bg-white hover:cursor-pointer px-10 py-2'>Design</li>
+              <Link className='text-xl text-gray-700 hover:bg-white hover:cursor-pointer px-10 py-2' to="/home">Home</Link>
+              <Link className='text-xl text-gray-700 hover:bg-white hover:cursor-pointer px-10 py-2' to="/create">Create</Link>
+              <Link className='text-xl text-gray-700 hover:bg-white hover:cursor-pointer px-10 py-2' to="/design">Design</Link>
             </ul>
           </nav>
-          {component}
+          <Routes>
+            <Route path="/home" element={<Home />} />
+            <Route path="/create" element={<Plain details={details} workExperiences={workExperiences} projects={projects} education={education} languages={languages} skills={skills}/>} />
+          </Routes>
         </div>
+      </Router>
       );
     };
     
